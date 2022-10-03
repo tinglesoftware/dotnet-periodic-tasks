eb0334e57522686e702bdbb029f3e49cef40cec1
Maxwell Weru <mburumaxwell@gmail.com>
2022-10-03 17:33:14 +0300

    Update XML comments in IPeriodicTaskRunner

--- src/Tingle.PeriodicTasks/IPeriodicTaskRunner.cs
+++ src/Tingle.PeriodicTasks/IPeriodicTaskRunner.cs
@@ -4,18 +4,18 @@
 public interface IPeriodicTaskRunner
 {
     /// <summary>Run the execution continuously based on the provided settings.</summary>
-    /// <param name="name"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="name">Name of the task.</param>
+    /// <param name="cancellationToken">A token to cancel execution.</param>
     /// <returns></returns>
     Task RunAsync(string name, CancellationToken cancellationToken);
 
     /// <summary>Execute the task once.</summary>
-    /// <param name="name"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="name">Name of the task.</param>
+    /// <param name="cancellationToken">A token to cancel execution.</param>
     /// <returns></returns>
     /// <remarks>
     /// Use this method if your application needs to execute the task
-    /// before the next schedule is reached or it the task is disabled.
+    /// before the next schedule is reached or if the task is disabled.
     /// </remarks>
     Task ExecuteAsync(string name, CancellationToken cancellationToken);
 }
