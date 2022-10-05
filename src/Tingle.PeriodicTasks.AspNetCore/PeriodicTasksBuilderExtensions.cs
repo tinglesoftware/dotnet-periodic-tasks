7d7c9d212501d0af2498887e9e704c8d940d2d2f
Maxwell Weru <mburumaxwell@gmail.com>
2022-10-05 11:04:19 +0300

    Exposed endpoint

---
+++ src/Tingle.PeriodicTasks.AspNetCore/PeriodicTasksBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Tingle.PeriodicTasks.AspNetCore;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods on <see cref="PeriodicTasksBuilder"/> for periodic tasks.
+/// </summary>
+public static class PeriodicTasksBuilderExtensions
+{
+    /// <summary>
+    /// Add support for exposing endpoints/routes on AspNetCore.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    public static PeriodicTasksBuilder AddAspNetCore(this PeriodicTasksBuilder builder)
+        => builder.AddAspNetCore(_ => { /* nothing to do here really */ });
+
+    /// <summary>
+    /// Add support for exposing endpoints/routes on AspNetCore.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="configure">A <see cref="PeriodicTasksAspNetCoreOptions"/> used to configure the health checks.</param>
+    /// <returns></returns>
+    public static PeriodicTasksBuilder AddAspNetCore(this PeriodicTasksBuilder builder, Action<PeriodicTasksAspNetCoreOptions> configure)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(configure);
+
+        var services = builder.Services;
+        services.Configure(configure);
+        services.AddScoped<PeriodicTasksEndpointsHandler>();
+
+        return builder;
+    }
+}
+
+///
+public class PeriodicTasksAspNetCoreOptions
+{
+
+}
---
+++ src/Tingle.PeriodicTasks.AspNetCore/PeriodicTasksEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Tingle.PeriodicTasks.AspNetCore;
+
+namespace Microsoft.AspNetCore.Builder;
+
+/// <summary>
+/// /// Provides extension methods for <see cref="IEndpointRouteBuilder"/> to add periodic tasks endpoints.
+/// </summary>
+public static class PeriodicTasksEndpointRouteBuilderExtensions
+{
+    /// <summary>
+    /// Maps incoming requests to the paths for periodic tasks.
+    /// </summary>
+    /// <param name="builder">The <see cref="IEndpointRouteBuilder"/> to add the routes to.</param>
+    /// <param name="prefix">The path prefix for the endpoints exposed.</param>
+    /// <returns>A <see cref="PeriodicTasksEndpointConventionBuilder"/> for endpoints associated with the service.</returns>
+    public static PeriodicTasksEndpointConventionBuilder MapPeriodicTasks(this IEndpointRouteBuilder builder, string prefix = "/periodic-tasks")
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        ValidateServicesRegistered(builder);
+
+        prefix = prefix.TrimEnd('/');
+        // in .NET 7 use route groups
+        var builders = new[]
+        {
+            builder.MapGet($"{prefix}/registrations", (PeriodicTasksEndpointsHandler handler) => handler.List()),
+            builder.MapGet($"{prefix}/registrations/{{name}}", (PeriodicTasksEndpointsHandler handler, string name) => handler.Get(name)),
+            builder.MapGet($"{prefix}/registrations/{{name}}/history", (PeriodicTasksEndpointsHandler handler, string name) => handler.GetHistory(name)),
+            builder.MapPost($"{prefix}/execute", (PeriodicTasksEndpointsHandler handler, PeriodicTaskExecutionRequest request) => handler.ExecuteOnDemandAsync(request)),
+        };
+
+        return new PeriodicTasksEndpointConventionBuilder(builders);
+    }
+
+    private static void ValidateServicesRegistered(IEndpointRouteBuilder endpoints)
+    {
+        if (endpoints.ServiceProvider.GetService(typeof(PeriodicTasksEndpointsHandler)) is null)
+        {
+            throw new InvalidOperationException("Unable to find the required services. Please add all the required services by calling " +
+                "'builder.AddAspNetCore' inside the call to 'services.AddPeriodicTasks(...)' in the application startup code.");
+        }
+    }
+}
