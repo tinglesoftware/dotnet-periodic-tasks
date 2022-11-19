747c65b0bbb6ae10204a6d3428c4f8caf9a84d19
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-19 13:15:04 +0300

    Added TriggerPeriodicTaskEvent and its consumer

---
+++ src/Tingle.PeriodicTasks.EventBus/TriggerPeriodicTaskEvent.cs
@@ -0,0 +1,23 @@
+﻿using Tingle.EventBus.Configuration;
+
+namespace Tingle.PeriodicTasks.EventBus;
+
+/// <summary>The event used for triggering periodic tasks via the EventBus.</summary>
+[EntityKind(EntityKind.Queue)]
+internal class TriggerPeriodicTaskEvent
+{
+    /// <summary>
+    /// The name of the registered task to execute.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Whether to await execution to complete.
+    /// </summary>
+    public bool Wait { get; set; }
+
+    /// <summary>
+    /// Whether to throw an exception if one is encountered.
+    /// </summary>
+    public bool Throw { get; set; }
+}
