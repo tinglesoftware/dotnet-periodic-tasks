cbd47bc05bc9ad847da5451907c8daaee4fd92ec
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-19 13:21:05 +0300

    Added extensions on EventBusBuilder to register the necessary services

---
+++ src/Tingle.PeriodicTasks.EventBus/EventBusBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Tingle.EventBus.Configuration;
+using Tingle.PeriodicTasks.EventBus;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>Extensions on <see cref="EventBusBuilder"/> for use with Periodic Tasks.</summary>
+public static class EventBusBuilderExtensions
+{
+    /// <summary>Register the necessary services for triggering periodic tasks.</summary>
+    /// <param name="builder">The <see cref="EventBusBuilder"/> to extend.</param>
+    /// <returns></returns>
+    public static EventBusBuilder AddPeriodicTasksTrigger(this EventBusBuilder builder)
+    {
+        return builder.AddConsumer<TriggerPeriodicTaskEventConsumer>();
+    }
+
+    /// <summary>Register the necessary services for triggering periodic tasks.</summary>
+    /// <param name="builder">The <see cref="EventBusBuilder"/> to extend.</param>
+    /// <param name="configure"></param>
+    /// <returns></returns>
+    public static EventBusBuilder AddPeriodicTasksTrigger(this EventBusBuilder builder, Action<EventRegistration, EventConsumerRegistration> configure)
+    {
+        return builder.AddConsumer<TriggerPeriodicTaskEventConsumer>(configure);
+    }
+}
