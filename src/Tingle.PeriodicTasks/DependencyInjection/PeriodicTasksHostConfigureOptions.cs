59e552e4c0e5ea9baadf1d1e9ee6fd92316531d7
Maxwell Weru <mburumaxwell@gmail.com>
2023-01-03 08:19:52 +0300

    Configure PeriodicTasksHostOptions from IConfiguration

--- src/Tingle.PeriodicTasks/DependencyInjection/PeriodicTasksHostConfigureOptions.cs
+++ src/Tingle.PeriodicTasks/DependencyInjection/PeriodicTasksHostConfigureOptions.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Tingle.PeriodicTasks;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 internal class PeriodicTasksHostConfigureOptions : IConfigureOptions<PeriodicTasksHostOptions>, IValidateOptions<PeriodicTasksHostOptions>
 {
     private readonly IHostEnvironment environment;
+    private readonly IPeriodicTasksConfigurationProvider configurationProvider;
 
-    public PeriodicTasksHostConfigureOptions(IHostEnvironment environment)
+    public PeriodicTasksHostConfigureOptions(IHostEnvironment environment, IPeriodicTasksConfigurationProvider configurationProvider)
     {
         this.environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        this.configurationProvider = configurationProvider ?? throw new ArgumentNullException(nameof(configurationProvider));
     }
 
     /// <inheritdoc/>
     public void Configure(PeriodicTasksHostOptions options)
     {
+        configurationProvider.Configuration.Bind(options);
+
+        // Set the default LockNamePrefix
         options.LockNamePrefix ??= environment.ApplicationName.ToLowerInvariant();
     }
 
