5dacb16fab3a96abd6b71c97def83d80bf4be70d
Maxwell Weru <mburumaxwell@gmail.com>
2023-01-03 08:25:12 +0300

    Move some logic to IPostConfigureOptions<T>

--- src/Tingle.PeriodicTasks/DependencyInjection/PeriodicTaskConfigureOptions.cs
+++ src/Tingle.PeriodicTasks/DependencyInjection/PeriodicTaskConfigureOptions.cs
@@ -5,7 +5,7 @@ using Tingle.PeriodicTasks;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-internal class PeriodicTaskConfigureOptions : IConfigureNamedOptions<PeriodicTaskOptions>, IValidateOptions<PeriodicTaskOptions>
+internal class PeriodicTaskConfigureOptions : IConfigureNamedOptions<PeriodicTaskOptions>, IPostConfigureOptions<PeriodicTaskOptions>, IValidateOptions<PeriodicTaskOptions>
 {
     private readonly PeriodicTasksHostOptions tasksHostOptions;
     private readonly IPeriodicTasksConfigurationProvider configurationProvider;
@@ -29,16 +29,24 @@ internal class PeriodicTaskConfigureOptions : IConfigureNamedOptions<PeriodicTas
 
         var configuration = configurationProvider.Configuration.GetSection($"Tasks:{name}");
         configuration.Bind(options);
+    }
+
+    public void PostConfigure(string? name, PeriodicTaskOptions options)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
 
         options.LockName ??= $"{tasksHostOptions.LockNamePrefix}:{name}";
         options.RetryPolicy ??= tasksHostOptions.DefaultRetryPolicy;
 
-        // try configure the description
-        var type = tasksHostOptions.Registrations[name];
-        var attrs = type.GetCustomAttributes(false);
-        options.Description ??= attrs.OfType<PeriodicTaskDescriptionAttribute>().SingleOrDefault()?.Description
-                             ?? attrs.OfType<DescriptionAttribute>().SingleOrDefault()?.Description
-                             ?? string.Empty; // makes sure it is visible in AspNetCore endpoint responses
+        // try configure the description if null
+        if (options.Description is null)
+        {
+            var type = tasksHostOptions.Registrations[name];
+            var attrs = type.GetCustomAttributes(false);
+            options.Description = attrs.OfType<PeriodicTaskDescriptionAttribute>().SingleOrDefault()?.Description
+                               ?? attrs.OfType<DescriptionAttribute>().SingleOrDefault()?.Description
+                               ?? string.Empty; // makes sure it is visible in AspNetCore endpoint responses
+        }
     }
 
     /// <inheritdoc/>
