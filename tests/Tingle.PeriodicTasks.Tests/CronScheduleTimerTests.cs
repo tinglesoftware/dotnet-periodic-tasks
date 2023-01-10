b061e05358b97296dbabdaefc4a3652de3d19b81
Maxwell Weru <mburumaxwell@gmail.com>
2023-01-10 12:41:29 +0300

    Added CronScheduleTimer

---
+++ tests/Tingle.PeriodicTasks.Tests/CronScheduleTimerTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tingle.PeriodicTasks.Tests;
+
+public class CronScheduleTimerTests
+{
+    [Fact]
+    public async Task CronScheduleTimer_Works()
+    {
+        var invocations = 0;
+        var schedule = "*/3 * * * * *"; // every 3 seconds
+        var timer = new CronScheduleTimer(schedule, async (t, ct) =>
+        {
+            Interlocked.Increment(ref invocations);
+            await t.StopAsync(ct);
+        });
+        await timer.StartAsync();
+        await Task.Delay(TimeSpan.FromSeconds(7));
+        Assert.Equal(1, invocations);
+    }
+}
