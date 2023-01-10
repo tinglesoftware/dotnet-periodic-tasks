b205f397152d5f0a63020b24f591f46269568a0e
Maxwell Weru <mburumaxwell@gmail.com>
2023-01-10 12:34:05 +0300

    Added DelayUtil

---
+++ src/Tingle.PeriodicTasks/Internal/DelayUtil.cs
@@ -0,0 +1,20 @@
+﻿namespace Tingle.PeriodicTasks.Internal;
+
+internal static class DelayUtil
+{
+    public static async Task DelayAsync(TimeSpan delay, CancellationToken cancellationToken)
+    {
+        // This method exists because we cannot delay for more than int.MaxValue so we have to split it into iterations
+        // Precision on the millisecond level is not guaranteed and is a rare necessity
+        var milliseconds = Convert.ToInt64(delay.TotalMilliseconds);
+        var iterations = (milliseconds - 1) / int.MaxValue;
+
+        while (iterations-- > 0)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(int.MaxValue), cancellationToken).ConfigureAwait(false);
+            milliseconds -= int.MaxValue;
+        }
+
+        await Task.Delay(TimeSpan.FromMilliseconds(milliseconds), cancellationToken).ConfigureAwait(false);
+    }
+}
