c98f18bf64fd1e3059342a9c77efab1512a950f7
Maxwell Weru <mburumaxwell@gmail.com>
2025-05-07 14:06:55 +0100

    Add `CronExpressionAttribute` for validation

---
+++ src/Tingle.PeriodicTasks/CronExpressionAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Specifies that a data field value is a valid CRON expression.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class CronExpressionAttribute : ValidationAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CronExpressionAttribute"/> class.
+    /// </summary>
+    public CronExpressionAttribute() : base("The field {0} must be a valid CRON Expression.") { }
+
+    /// <inheritdoc/>
+    public override bool IsValid(object? value)
+    {
+        if (value is not string s || string.IsNullOrEmpty(s)) return true;
+
+        return Tingle.PeriodicTasks.CronSchedule.TryParse(value: s, out _);
+    }
+}
---
+++ tests/Tingle.PeriodicTasks.Tests/CronExpressionAttributeTests.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tingle.PeriodicTasks.Tests;
+
+public class CronExpressionAttributeTests
+{
+    [Theory]
+    [InlineData("0 2 * * 1-5", true)]
+    [InlineData("14 10 2 * * 1-5", true)] // with seconds
+    [InlineData(null, true)]
+    [InlineData("", true)]
+    [InlineData("A", false)]
+    public void CronExpression_Validation_Works(string? testValue, bool expected)
+    {
+        var obj = new TestModel(testValue);
+        var context = new ValidationContext(obj);
+        var results = new List<ValidationResult>();
+        var actual = Validator.TryValidateObject(obj, context, results, true);
+        Assert.Equal(expected, actual);
+
+        // if expected it to pass, the results should be empty
+        if (expected) Assert.Empty(results);
+        else
+        {
+            var val = Assert.Single(results);
+            var memeberName = Assert.Single(val.MemberNames);
+            Assert.Equal(nameof(TestModel.SomeValue), memeberName);
+            Assert.NotNull(val.ErrorMessage);
+            Assert.NotEmpty(val.ErrorMessage);
+            Assert.EndsWith("must be a valid CRON Expression.", val.ErrorMessage);
+        }
+    }
+
+    record TestModel([property: CronExpression] string? SomeValue);
+}
